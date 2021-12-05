f08281a1d5a907e4107ca4f2001a4f5c9b08f7b4
ChahineElati <chahinosaiyan@gmail.com>
2021-12-05 22:07:41 +0100

    admin - ajouter une formation

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react'
-import { Container } from 'semantic-ui-react'
-import { Menu,Input } from 'semantic-ui-react'
+import { Menu,Input, Segment } from 'semantic-ui-react'
 export default class Admin extends Component {
-    render() {
-        return (
-            <div>
-                <Container textAlign='center'>
-                <p style={{
-                        color: "white",
-                        fontSize: 30,
-                        paddingBottom: 20,
-                        backgroundColor: 'rgba(17, 95, 249, 0.8)',
-                        borderRadius: 5,
-                        fontWeight: 900,
-                        margin: 30,
-                        paddingLeft: 50,
-                        paddingTop: 25,
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        }}>
-                            Bienvenue au espace Admin
-                            </p>
-                            
-                </Container>
-            </div>
-        )
-    }
-    state = { activeItem: 'admin' }
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
-  
+  state = { }
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
     render() {
       const { activeItem } = this.state
-  
+      var jsx;
+      if (activeItem ==='ajouter une formation ') {
+        jsx =<Segment attached='bottom'><p>Hello</p></Segment>;
+      }
       return (
-        <Menu tabular>
+        <div>
+        <Menu attached='top' tabular>
           <Menu.Item
-            name='liste de formation '
+            name='liste de formation'
             active={activeItem === 'liste de formation'}
             onClick={this.handleItemClick}
            />
@@ -49,6 +28,8 @@ export default class Admin extends Component {
             onClick={this.handleItemClick}
           />
         </Menu>
+          {jsx}
+        </div>
       )
     }
     
