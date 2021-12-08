c9ea271788ea1a5a4e17bc3990353c148d86d94c
louay <benazizalouay6@gmail.com>
2021-12-08 17:27:36 +0100

    admin

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react'
 import { Menu,Input, Segment } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
+
+
+
+   const options = [
+  { key: '1', text: 'type1', value: 'type1' },
+  { key: '2', text: 'type2', value: 'type2' },
+  { key: '3', text: 'type3', value: 'type3' },
+]  
+
 export default class Admin extends Component {
+
 
   state = { }
 
+
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+    handleChange = (e, { value }) => this.setState({ value });
+
 
     render() {
       const { activeItem } = this.state
+      const { value } = this.state
       var jsx;
       if (activeItem ==='ajouter une formation ') {
-        jsx =<Segment attached='bottom'><p>Hello</p></Segment>;
+        jsx =<Segment attached='bottom'><p>formulaire</p>
+              <Form>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='titre de formation' placeholder='titre de formation' />
+          <Form.Input fluid label='nom de formateur' placeholder='nom de formateur ' />
+          <Form.Select
+            fluid
+            label='type de formation'
+            options={options}
+            placeholder='type de formation'
+          />
+        </Form.Group>
+        <Form.Group inline>
+          <label>date</label>
+          
+          
+        </Form.Group>
+        <Form.TextArea label='About' placeholder='description du formation...' />
+        
+        <Form.Button>Submit</Form.Button>
+      </Form>
+
+
+
+        </Segment>;
       }
       return (
         <div>
